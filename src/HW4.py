5c98a1aecaac0f955b6003a10bf0b558101a88fe
bolshchikov <sergey@bolshchikov.net>
2012-12-28 16:54:18 +0200

    HW4.py added

---
+++ src/HW4.py
@@ -0,0 +1,20 @@
+class HW4:
+	'''
+	You are to implement the two functions in this class.
+	You can add any other method or members to it as well.
+	However, you cannot change their signature. 
+	'''
+	def canDemandBeAnswered(self, root):
+		'''
+		This method will return true if the tree rooted at node sn can answer
+        the demand induced by its leaves.
+		'''
+		pass
+
+	def getBestCustomers(self, root):
+		'''
+		This method should return a linked list of nodes representing the 
+        customers with the overall highest revenue.
+        The resulting list should conform to the capacity limitations.
+		'''
+		pass 
